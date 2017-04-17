9c7178ff1c1413982394502d17400ef772b65b8e
Łukasz Durniat <ldurniat@gmail.com>
2017-04-17 15:38:07 +0200

    Add new module

--- main.lua
+++ main.lua
@@ -6,6 +6,9 @@ local composer   = require( 'composer' )
 local app        = require( 'lib.app' )
 local preference = require( 'preference' ) 
 
+-- Dodaje nowe funkcje do standardowych modułów 
+require( 'lib.utils' ) 
+
 -- Ładowanie ustawień z pliku settings.json
 preference:load()
 
