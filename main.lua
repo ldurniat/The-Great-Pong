4199b5a4a2cc0afee2abe41a3f841103d43a900f
Łukasz Durniat <ldurniat@gmail.com>
2017-04-21 16:15:52 +0200

    Add Ponywolf memory monitor

--- main.lua
+++ main.lua
@@ -9,6 +9,46 @@ local preference = require( 'preference' )
 -- Dodaje nowe funkcje do standardowych modułów 
 require( 'lib.utils' ) 
 
+-- Usunięcie paska navigacyjnego z dołu
+if system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 then
+  native.setProperty( "androidSystemUiVisibility", "lowProfile" )
+else
+  native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
+end
+
+-- are we running on a simulator?
+local isSimulator = "simulator" == system.getInfo( "environment" )
+local isMobile = ("ios" == system.getInfo("platform")) or ("android" == system.getInfo("platform"))
+
+-- if we are load our visual monitor that let's a press of the "F"
+-- key show our frame rate and memory usage, "P" to show physics
+if isSimulator then 
+
+  -- show FPS
+  local visualMonitor = require( "com.ponywolf.visualMonitor" )
+  local visMon = visualMonitor:new()
+  visMon.isVisible = true
+
+  -- show/hide physics
+  local function debugKeys( event )
+    local phase = event.phase
+    local key = event.keyName
+    if phase == "up" then
+      if key == "p" then
+        physics.show = not physics.show
+        if physics.show then 
+          physics.setDrawMode( "hybrid" ) 
+        else
+          physics.setDrawMode( "normal" )  
+        end
+      elseif key == "f" then
+        visMon.isVisible = not visMon.isVisible 
+      end
+    end
+  end
+  Runtime:addEventListener( "key", debugKeys )
+end
+
 -- Ładowanie ustawień z pliku settings.json
 preference:load()
 
