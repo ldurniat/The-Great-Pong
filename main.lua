761146117242587ca852f272c98c8b5657e6e294
Łukasz Durniat <ldurniat@gmail.com>
2017-05-27 22:32:47 +0200

    Add language settings to map

--- main.lua
+++ main.lua
@@ -21,9 +21,9 @@ _CY = display.contentCenterY
 
 -- Usunięcie paska navigacyjnego z dołu
 if system.getInfo( "androidApiLevel" ) and system.getInfo( "androidApiLevel" ) < 19 then
-  native.setProperty( "androidSystemUiVisibility", "lowProfile" )
+ 	native.setProperty( "androidSystemUiVisibility", "lowProfile" )
 else
-  native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
+ 	native.setProperty( "androidSystemUiVisibility", "immersiveSticky" ) 
 end
 
 -- czy uruchomiony w simulatorze ?
@@ -33,45 +33,44 @@ local isMobile = app.isAndroid or app.isiOS
 -- jezeli widoczny to przycisk 'F' wyświetla fps oraz zuzycie pamięci
 -- przycisk 'P' wyświetla fizykę w trybie debug 
 if isSimulator then 
+	-- show FPS
+	local visualMonitor = require( "com.ponywolf.visualMonitor" )
+	local visMon = visualMonitor:new()
+	visMon.isVisible = false
 
-  -- show FPS
-  local visualMonitor = require( "com.ponywolf.visualMonitor" )
-  local visMon = visualMonitor:new()
-  visMon.isVisible = false
-
-  -- wyświetl/ukryj fizykę
-  local function debugKeys( event )
-    local phase = event.phase
-    local key = event.keyName
-    if phase == "up" then
-      if key == "p" then
-        physics.show = not physics.show
-        if physics.show then 
-          physics.setDrawMode( "hybrid" ) 
-        else
-          physics.setDrawMode( "normal" )  
-        end
-      elseif key == "f" then
-        visMon.isVisible = not visMon.isVisible 
-      end
-    end
-  end
-  Runtime:addEventListener( "key", debugKeys )
+	-- wyświetl/ukryj fizykę
+	local function debugKeys( event )
+		local phase = event.phase
+		local key = event.keyName
+		if phase == "up" then
+			if ( key == "p" ) then
+				physics.show = not physics.show
+				if physics.show then 
+				 	physics.setDrawMode( "hybrid" ) 
+				else
+				 	physics.setDrawMode( "normal" )  
+				end
+			elseif ( key == "f" ) then
+				visMon.isVisible = not visMon.isVisible 
+			end
+		end
+	end
+	Runtime:addEventListener( "key", debugKeys )
 end
 
 local function onSystemEvent( event )    
-    if (event.type == "applicationStart") then
+	if (event.type == "applicationStart") then
 
-    elseif (event.type == "applicationExit") then 
-        -- Zapis ustawień przed zamknięciem aplikacji
-        preference:save()
-        -- Usunięcie 
-        app.disposeSounds()
-    elseif ( event.type == "applicationSuspend" ) then
-      
-    elseif event.type == "applicationResume" then
-        
-    end
+	elseif (event.type == "applicationExit") then 
+		-- Zapis ustawień przed zamknięciem aplikacji
+		preference:save()
+		-- Usunięcie 
+		app.disposeSounds()
+	elseif ( event.type == "applicationSuspend" ) then
+	  
+	elseif event.type == "applicationResume" then
+		
+	end
 end
 
 Runtime:addEventListener( "system", onSystemEvent )
